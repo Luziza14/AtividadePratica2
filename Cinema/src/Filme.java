5a892b2c2f5086e89f6d21a400faa7ff0142fc02
Fernanda Piekas <fepiekas@gmail.com>
2024-04-25 22:37:06 -0300

    SuperClasse

--- Cinema/src/Filme.java
+++ Cinema/src/Filme.java
@@ -1,3 +1,40 @@
 public class Filme {
     
+    private String nome;
+    private String genero;
+
+    public Filme(){
+
+    }
+
+    public Filme(String nome, String genero) {
+        this.nome = nome;
+        this.genero = genero;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getGenero() {
+        return genero;
+    }
+
+    public void setGenero(String genero) {
+        this.genero = genero;
+    }
+
+    @Override
+    public String toString(){
+
+        return "Filme em cartaz: " + nome + "\n Gênero: " + genero;
+    }
+
+
 }
+
+
