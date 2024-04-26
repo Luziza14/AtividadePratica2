45751f8174607617a9cb038794bad129736b686a
Fernanda Luiza <141834497+Luziza14@users.noreply.github.com>
2024-04-26 11:29:25 -0300

    Criação Cadastro de filmes

--- Cinema/src/Cadastro.java
+++ Cinema/src/Cadastro.java
@@ -1,2 +1,31 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class Cadastro {
+
+private static List<Filme> listaSessao = new ArrayList<>();
+
+public static void cadastrar (Filme filme){
+    
+    listaSessao.add(filme);
+}
+
+public static List<Filme> getlistaSessao(){
+
+    return listaSessao;
+}
+
+public static Filme buscar(String nome){
+
+    for(Filme fil : listaSessao){
+
+        if (temp.getNome() == nome){
+
+            return fil;
+        }
+        
+    }
+
+    return null;
+}
 }
