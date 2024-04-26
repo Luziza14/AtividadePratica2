10d5bf0a05cf1fa399c85a47278fbab0cadfb845
Victor de Luca Victor <victorxdobro@gmail.com>
2024-04-25 23:23:12 -0300

    Alteração classe cliente

--- Cinema/src/Cliente.java
+++ Cinema/src/Cliente.java
@@ -1,3 +1,34 @@
-public class Cliente {
+public class Cliente extends Sala{
+
     
-}
+
+        String nomeCliente;
+        
+    
+        
+            public Cliente() {
+            }
+        
+            public Cliente (String nomeCliente, String nome, int id, int idSala) {
+                super(nome,id,idSala);
+                this.nomeCliente = nomeCliente;
+            }
+        
+
+            public String getNomeCliente() {
+                return nomeCliente;
+            }
+
+            public void setNomeCliente(String nomeCliente) {
+                this.nomeCliente = nomeCliente;
+            }
+
+            @Override
+            public String toString() {
+        
+                return super.toString() +
+                        "\nNome do projeto: " + nomeCliente;
+            }
+
+        
+        }
