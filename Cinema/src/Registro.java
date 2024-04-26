c71ed4c03566c48590a8082814597a8d24e91684
Victor de Luca Victor <victorxdobro@gmail.com>
2024-04-25 23:09:33 -0300

    Alteração classe Registro

--- Cinema/src/Registro.java
+++ Cinema/src/Registro.java
@@ -1,3 +1,32 @@
-public class Registro {
+public class Registro extends Filme {
+
+    int id;
     
-}
+
+    
+        public Registro() {
+        }
+    
+        public Registro(String nome, String genero, int id) {
+            super(nome, genero);
+            this.id = id;
+        }
+    
+
+        public int getId() {
+            return id;
+        }
+
+        public void setId(int id) {
+            this.id = id;
+        }
+
+        @Override
+        public String toString() {
+    
+            return super.toString() +
+                    "\nNome do projeto: " + id;
+        }
+    
+    }
+
